22571b6c8f2dc83295415dc74b79b137ececea6e
gersonUb <gersoncabj@gmail.com>
2026-01-26 20:39:43 -0300

    Initialize project: Domain Entities (User, Subject, Resource)

---
+++ domain/src/entities/user.py
@@ -0,0 +1,10 @@
+from uuid import UUID, uuid4
+from typing import List
+from pydantic import BaseModel, EmailStr, Field
+from .subject import Subject
+
+class User(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
+    username: str
+    email: EmailStr
+    subjects: List[Subject] = Field(default_factory=list)
