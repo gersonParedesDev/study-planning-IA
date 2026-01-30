39b45750833c9463ca484d83bb62de138ccc1c95
gersonUb <gersoncabj@gmail.com>
2026-01-30 19:21:46 -0300

    feat: add config database sqlite

--- infrastructure/src/database/config.py
+++ infrastructure/src/database/config.py
@@ -1,17 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Usamos SQLite por ahora (se crea un archivo 'app.db' en tu carpeta)
 DATABASE_URL = "sqlite:///./app.db"
 
-# 1. El Engine es el motor de conexión
 engine = create_engine(
     DATABASE_URL, 
-    connect_args={"check_same_thread": False} # Solo necesario para SQLite
+    connect_args={"check_same_thread": False}
 )
 
-# 2. SessionLocal es la fábrica de sesiones para cada petición
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# 3. Base para crear los modelos (Tablas)
 Base = declarative_base()
