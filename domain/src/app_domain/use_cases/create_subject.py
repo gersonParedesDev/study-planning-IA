64df47c08234e157ddcf3c5b9f98c3c27c354512
gersonUb <gersoncabj@gmail.com>
2026-03-18 11:25:44 -0300

    refactor: create subject add text extractor

--- domain/src/app_domain/use_cases/create_subject.py
+++ domain/src/app_domain/use_cases/create_subject.py
@@ -2,12 +2,16 @@ from app_domain.dtos.input.create_subject_dto import CreateSubjectDTO
 from app_domain.entities.subject import Subject
 from app_domain.entities.resource import Resource
 from app_domain.dtos.output.subject_response_dto import ResourceOutputDTO, SubjectResponseDTO
+from app_domain.ports.output.document_extractor import DocumentExtractor
+from app_domain.ports.output.file_storage import FileStoragePort
 import uuid
 
 class CreateSubjectUseCase:
-    def __init__(self, subject_repository, resource_repository):
+    def __init__(self, subject_repository, resource_repository, extractor: DocumentExtractor, storage: FileStoragePort):
         self.subject_repo = subject_repository
         self.resource_repo = resource_repository
+        self.extractor = extractor
+        self.storage = storage
 
     def execute(self, data: CreateSubjectDTO) -> SubjectResponseDTO:
         if self.subject_repo.get_by_name(data.name):
@@ -26,6 +30,16 @@ class CreateSubjectUseCase:
         saved_resources = []
 
         for r in data.resources:
+            extracted_text = None
+            try:
+                file_bytes = self.storage.get_file_bytes(r.file_url)
+                if self.extractor.is_image(r.filename):
+                    extracted_text = self.extractor.extract_from_image(file_bytes)
+                else:
+                    extracted_text = self.extractor.extract_from_pdf(file_bytes)
+            except Exception:
+                extracted_text = None
+
             resource = Resource(
                 id=uuid.uuid4(),
                 subject_id=subject_id,
@@ -33,9 +47,9 @@ class CreateSubjectUseCase:
                 file_url=r.file_url,
                 filename=r.filename,
                 resource_type=r.resource_type,
+                extracted_text=extracted_text,
             )
             self.resource_repo.save(resource)
-
             saved_resources.append(resource)
 
         return SubjectResponseDTO(
